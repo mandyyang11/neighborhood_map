20265082b2f0b02af2c987da3c68e0462e20490d
mandyyang11 <mandyyang11@yahoo.com>
2017-04-23 14:34:52 -0700

    feat: asynchronously load yelp data
    Yelp data is being loaded asynchronously on the background. Also need
    to deal with the case when its not already loaded and update the
    information when its loaded.

--- static/js/view_model.js
+++ static/js/view_model.js
@@ -1,3 +1,55 @@
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// These marker functions (except some part of addMarker()) came directly from google map totural
+// please refer to https://developers.google.com/maps/documentation/javascript/examples/marker-remove
+//
+//
+var map;
+var markers = [];
+var masterInfoWindow;
+
+// Adds a marker to the map and push to the array.
+function addMarker(location, idx) {
+    
+    // mark the map with given location
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+    
+    // setting up infowindow on the click
+    marker.addListener('click', function() {
+        
+        // checking if the yelp data is being loaded
+        if (yelp.length > idx){
+            
+            // if yelp data is loaded, display the information in the infowindow
+            masterInfoWindow.setContent(yelp[idx]);
+        }else{
+            
+            // if the data is not there, display the name and ask user to try again
+            masterInfoWindow.setContent('<div><b>' + initRestaurants[idx].name +
+                        '</b></div>' +
+                        '<div>The infomation still loading...</div>' +
+                        '<div>Please try again.</div>');
+        }
+        
+        // infowindow.open(map, marker);
+        masterInfoWindow.open(map, marker);
+
+        // set up bounce animation on the click as one of the project requirements
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            marker.setAnimation(null);
+        }, 700);
+    });
+    
+    markers.push(marker);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 // statically define the list of restaurants
 var initRestaurants = [
     {name: 'Eureka!', lat: 37.8700698, long: -122.269054, yelpId: 'eureka-berkeley-3'},
@@ -11,6 +63,31 @@ var initRestaurants = [
     {name: 'My Tofu House', lat: 37.7806363, long: -122.4713094, yelpId: 'my-tofu-house-san-francisco'},
     {name: 'Mensho Tokyo', lat: 37.7867071, long: -122.4165156, yelpId: 'mensho-tokyo-san-francisco-2'}
 ];
+
+// load up yelp data asynchronouslly
+var yelp = [];
+for (var i in initRestaurants){
+    
+    // create yelp endpoint string
+    var yelpEndpoint = 'http://localhost:5000/yelp_api/'+ initRestaurants[i].yelpId;
+    
+    // query yelp data with jQuery
+    $.getJSON(yelpEndpoint).done(function(data) {
+        
+        // generate info string from the data
+        var info = '<div><b>' + data.name + '</b></div>' +
+            '<div> Yelp Rating: ' + data.rating + '</div>' +
+            '<div>' + data.address + '</div>' +
+            '<div>' + data.city + '</div>';
+                    
+        // push the info to yelp array
+        yelp.push(info);
+    
+    // handle api failure
+    }).fail(function() {
+        alert("There was an error with the Yelp API call.");
+    });
+}
 
 // create the view model and the bindings
 function loadModel() {
